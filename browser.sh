ce15f06442b21ced280d082ff0be4757e8c2a502
nima37 <132191693+nima37@users.noreply.github.com>
2024-11-19 00:08:53 +0330

    Update browser.sh

--- browser.sh
+++ browser.sh
@@ -31,8 +31,8 @@ install_chromium() {
             -e CUSTOM_USER=$USERNAME \
             -e PASSWORD=$PASSWORD \
             -e CHROME_CLI=https://www.youtube.com/@IR_TECH/ `#optional` \
-            -p 3000:3000 \
-            -p 3001:3001 \
+            -p 3009:3009 \
+            -p 3010:3010 \
             -v /root/chromium/config:/config \
             --shm-size="1gb" \
             --restart unless-stopped \
@@ -41,7 +41,7 @@ install_chromium() {
         echo "Chromium installed successfully."
         IP=$(hostname -I | awk '{print $1}')
         echo " "
-        echo "Use browser with http://$IP:3000"
+        echo "Use browser with http://$IP:3009"
     fi
 }
 
@@ -74,8 +74,8 @@ install_firefox() {
             -e TZ=Etc/UTC \
             -e CUSTOM_USER=$USERNAME \
             -e PASSWORD=$PASSWORD \
-            -p 4000:3000 \
-            -p 4001:3001 \
+            -p 4000:3009 \
+            -p 4001:3010 \
             -v /root/firefox/config:/config \
             --shm-size="1gb" \
             --restart unless-stopped \
